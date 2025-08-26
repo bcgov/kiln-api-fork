97d470321886e24a0bcc527e13022b7fd6d291e9
Danilo Meireles <danilo.meireles@gmail.com>
2025-08-26 15:21:41 -0700

    resolve eslint issues

--- test/default.controller.test.ts
+++ test/default.controller.test.ts
@@ -9,8 +9,11 @@ describe('Default Controller', () => {
       .get('/api/')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(res => {
-        expect(res.body).to.have.property('status', 'Kiln API Gateway is running.');
+      .then((res) => {
+        expect(res.body).to.have.property(
+          'status',
+          'Kiln API Gateway is running.'
+        );
       }));
 
   it('should handle create', () =>
@@ -19,7 +22,7 @@ describe('Default Controller', () => {
       .send({ name: 'test' })
       .expect('Content-Type', /json/)
       .expect(201)
-      .then(res => {
+      .then((res) => {
         expect(res.body).to.have.property('message', 'Create endpoint hit');
         expect(res.body.data).to.have.property('name', 'test');
       }));
@@ -29,7 +32,7 @@ describe('Default Controller', () => {
       .get('/api/123')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(res => {
+      .then((res) => {
         expect(res.body).to.have.property('message').that.includes('123');
       }));
 });
